bf637749d1a0e4d1e3046a8bc84bb3122809ff4b
Omar Shokr <99566895+madaraaa3@users.noreply.github.com>
2023-05-11 23:27:24 +0300

    Add files via upload

---
+++ speller/dictionary.c
@@ -0,0 +1,75 @@
+// Implements a dictionary's functionality
+
+#include <ctype.h>
+#include <stdbool.h>
+
+#include "dictionary.h"
+
+// Represents a node in a hash table
+typedef struct node
+{
+    char word[LENGTH + 1];
+    struct node *next;
+}
+node;
+
+// TODO: Choose number of buckets in hash table
+const unsigned int N = 26;
+
+// Hash table
+node *table[N];
+
+// Returns true if word is in dictionary, else false
+bool check(const char *word)
+{
+    // TODO
+    return false;
+}
+
+// Hashes word to a number
+unsigned int hash(const char *word)
+{
+    // TODO: Improve this hash function
+    return toupper(word[0]) - 'A';
+}
+
+// Loads dictionary into memory, returning true if successful, else false
+bool load(const char *dictionary)
+{
+    // TODO
+    FILE * dictionary;
+    fopen(*dictionary, "r");
+    if (*dictionary == NULL)
+    {
+        return false;
+    }
+    char word[LENGTH];
+    while((word = fscanf(dictionary, "%S", word)) != EOF)
+    {
+        fscanf(dictionary, "%S", word);
+        node *new = malloc(sizeof(node));
+        if (new == NULL)
+        {
+            return false;
+        }
+        strcpy(new->word, word);
+        int res;
+        res = hash(word);
+    }
+
+    return false;
+}
+
+// Returns number of words in dictionary if loaded, else 0 if not yet loaded
+unsigned int size(void)
+{
+    // TODO
+    return 0;
+}
+
+// Unloads dictionary from memory, returning true if successful, else false
+bool unload(void)
+{
+    // TODO
+    return false;
+}
